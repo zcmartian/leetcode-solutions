df6e6afa3ea09b83a940886e69bdbd0eb8598bfc
zcmartian <zcmit@126.com>
2019-01-13 21:04:52 +0800

    add 896

--- 896.monotonic-array.java
+++ 896.monotonic-array.java
@@ -83,9 +83,30 @@ class Solution {
     public boolean isMonotonic(int[] A) {
         boolean isSet = false;
         int direction = 0;
-        int length = A.length; 
+        int length = A.length;
         if (length <= 2) {
             return true;
         }
+        for (int i = 0;i<A.length-1;i++) {
+            if (!isSet) {
+                if (A[i] == A[i+1]) {
+                    continue;
+                } else {
+                    direction = A[i]<A[i+1] ? -1 : 1;
+                    isSet = true;
+                }
+            } else {
+                if (direction == 1) {
+                    if (A[i]<A[i+1]) {
+                        return false;
+                    }
+                } else {
+                    if (A[i]>A[i+1]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
