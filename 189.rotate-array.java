8a79d515f9e7366aa988c5e782dc6a31d3254f43
zhouchao3 <zhouchao3@xiaomi.com>
2019-01-17 18:04:15 +0800

    add 189, use 3 rotates

---
+++ 189.rotate-array.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=189 lang=java
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (28.27%)
+ * Total Accepted:    250.6K
+ * Total Submissions: 886.6K
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation: 
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * 
+ */
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int length = nums.length;
+        k = k % length;
+        doRotate(nums, 0, length-k-1);
+        doRotate(nums, length-k, length-1);
+        doRotate(nums, 0, length-1);
+    }
+
+    private void doRotate(int[] nums, int left, int right) {
+        while(left < right) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+}
