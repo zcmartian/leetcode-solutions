a0a4a3b83289dafd9e49ce2e101e91669083833a
zhouchao3 <zhouchao3@xiaomi.com>
2019-01-22 10:26:41 +0800

    add 922, use two pointers

---
+++ 922.sort-array-by-parity-ii.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=922 lang=python
+#
+# [922] Sort Array By Parity II
+#
+# https://leetcode.com/problems/sort-array-by-parity-ii/description/
+#
+# algorithms
+# Easy (66.34%)
+# Total Accepted:    23.9K
+# Total Submissions: 36K
+# Testcase Example:  '[4,2,5,7]'
+#
+# Given an array A of non-negative integers, half of the integers in A are odd,
+# and half of the integers are even.
+# 
+# Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is
+# even, i is even.
+# 
+# You may return any answer array that satisfies this condition.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: [4,2,5,7]
+# Output: [4,5,2,7]
+# Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been
+# accepted.
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# 2 <= A.length <= 20000
+# A.length % 2 == 0
+# 0 <= A[i] <= 1000
+# 
+# 
+# 
+# 
+# 
+#
+class Solution(object):
+    def sortArrayByParityII(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        i = 0
+        j = 1
+        n = len(A)
+
+        while i < n and j < n:
+            if A[i] % 2 == 0:
+                i += 2
+            elif A[j] % 2 == 1:
+                j += 2
+            else:
+                A[i], A[j] = A[j], A[i]
+                i += 2
+                j += 2
+
+        return A
+        
