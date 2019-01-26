74b003ef5df50adc034e2597c557acb53a1d4f31
zcmartian <zcmit@126.com>
2019-01-26 18:16:44 +0800

    add 21 use loop

---
+++ 21.merge-two-sorted-lists.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (45.17%)
+ * Total Accepted:    484.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list
+ * should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * 
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode ret = head;
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val){
+                head.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                head.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+
+        while(l1 != null) {
+            head.next = new ListNode(l1.val);
+            l1 = l1.next;
+            head = head.next;
+        }
+        while(l2 != null) {
+            head.next = new ListNode(l2.val);
+            l2 = l2.next;
+            head = head.next;
+        }
+        return ret.next;
+    }
+}
