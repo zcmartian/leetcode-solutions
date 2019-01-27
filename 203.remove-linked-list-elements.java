568f8da1e515a0269a352d009505299a15aa57fa
zcmartian <zcmit@126.com>
2019-01-27 16:49:11 +0800

    add 203 java

---
+++ 203.remove-linked-list-elements.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=203 lang=java
+ *
+ * [203] Remove Linked List Elements
+ *
+ * https://leetcode.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (35.04%)
+ * Total Accepted:    199.7K
+ * Total Submissions: 570K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * Remove all elements from a linked list of integers that have value val.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null) {
+            if(cur.val == val) {
+                if(pre == null) {
+                    head = cur.next;
+                } else {
+                    pre.next = cur.next;
+                }
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
