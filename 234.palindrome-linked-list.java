aafb5c270558f0e908b04ef701b51be9b87aada0
zcmartian <zcmit@126.com>
2019-01-27 15:57:16 +0800

    add 234, ues reverse half list

---
+++ 234.palindrome-linked-list.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=234 lang=java
+ *
+ * [234] Palindrome Linked List
+ *
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (35.04%)
+ * Total Accepted:    221.8K
+ * Total Submissions: 633K
+ * Testcase Example:  '[1,2]'
+ *
+ * Given a singly linked list, determine if it is a palindrome.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2
+ * Output: false
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->2->2->1
+ * Output: true
+ * 
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        while(slow != null){
+            if(slow.val != fast.val)
+                return false;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
