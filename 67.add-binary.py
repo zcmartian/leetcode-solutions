e90564ec12e0d5c2b4b640c32ddbd4138010d0e2
zhouchao3 <zhouchao3@xiaomi.com>
2019-01-30 16:24:12 +0800

    add 67 python recursive

---
+++ 67.add-binary.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=67 lang=python
+#
+# [67] Add Binary
+#
+# https://leetcode.com/problems/add-binary/description/
+#
+# algorithms
+# Easy (37.48%)
+# Total Accepted:    267K
+# Total Submissions: 712.4K
+# Testcase Example:  '"11"\n"1"'
+#
+# Given two binary strings, return their sum (also a binary string).
+# 
+# The input strings are both non-empty and contains only characters 1 or 0.
+# 
+# Example 1:
+# 
+# 
+# Input: a = "11", b = "1"
+# Output: "100"
+# 
+# Example 2:
+# 
+# 
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+# 
+#
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        if len(a)==0: return b
+        if len(b)==0: return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[0:-1],b[0:-1]),'1')+'0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[0:-1],b[0:-1])+'0'
+        else:
+            return self.addBinary(a[0:-1],b[0:-1])+'1'
