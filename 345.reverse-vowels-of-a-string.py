735beac43364ab967bf5536a118ddd2fdad81267
zhouchao3 <zhouchao3@xiaomi.com>
2019-01-30 09:56:54 +0800

    add 345 python re ?i

---
+++ 345.reverse-vowels-of-a-string.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=345 lang=python
+#
+# [345] Reverse Vowels of a String
+#
+# https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+#
+# algorithms
+# Easy (40.61%)
+# Total Accepted:    138.9K
+# Total Submissions: 342.1K
+# Testcase Example:  '"hello"'
+#
+# Write a function that takes a string as input and reverse only the vowels of
+# a string.
+# 
+# Example 1:
+# 
+# 
+# Input: "hello"
+# Output: "holle"
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: "leetcode"
+# Output: "leotcede"
+# 
+# 
+# Note:
+# The vowels does not include the letter "y".
+# 
+# 
+# 
+#
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = re.findall('(?i)[aeiou]', s)        
+        return re.sub('(?i)[aeiou]', lambda m: vowels.pop(), s)
