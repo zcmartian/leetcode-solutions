67fa245d6cea536a7cc9d6637539c6dd3e2bbf39
zhouchao3 <zhouchao3@xiaomi.com>
2019-01-31 11:30:44 +0800

    add 125

---
+++ 125.valid-palindrome.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (29.84%)
+ * Total Accepted:    309.7K
+ * Total Submissions: 1M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases.
+ * 
+ * Note: For the purpose of this problem, we define empty string as valid
+ * palindrome.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "race a car"
+ * Output: false
+ * 
+ * 
+ */
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if(!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+
+        return true;
+
+    }
+}
