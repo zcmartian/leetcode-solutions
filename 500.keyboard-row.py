8c03408a40dfb697c9c0213db6d772e28499b70c
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-01 09:45:30 +0800

    add 500 python

---
+++ 500.keyboard-row.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=500 lang=python
+#
+# [500] Keyboard Row
+#
+# https://leetcode.com/problems/keyboard-row/description/
+#
+# algorithms
+# Easy (61.42%)
+# Total Accepted:    80.4K
+# Total Submissions: 130.9K
+# Testcase Example:  '["Hello","Alaska","Dad","Peace"]'
+#
+# Given a List of words, return the words that can be typed using letters of
+# alphabet on only one row's of American keyboard like the image below.
+# 
+# 
+# 
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input: ["Hello", "Alaska", "Dad", "Peace"]
+# Output: ["Alaska", "Dad"]
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# You may use one character in the keyboard more than once.
+# You may assume the input string will only contain letters of alphabet.
+# 
+# 
+#
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        return filter(re.compile('(?i)([qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*)$').match, words) 
