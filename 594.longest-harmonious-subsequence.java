9873ed3c9bc1fdd2f5ac5e94d49885c62813a758
zcmartian <zcmit@126.com>
2019-02-11 14:23:21 +0800

    add 594

---
+++ 594.longest-harmonious-subsequence.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=594 lang=java
+ *
+ * [594] Longest Harmonious Subsequence
+ *
+ * https://leetcode.com/problems/longest-harmonious-subsequence/description/
+ *
+ * algorithms
+ * Easy (42.80%)
+ * Total Accepted:    31.8K
+ * Total Submissions: 74.4K
+ * Testcase Example:  '[1,3,2,2,5,2,3,7]'
+ *
+ * We define a harmonious array is an array where the difference between its
+ * maximum value and its minimum value is exactly 1.
+ * 
+ * Now, given an integer array, you need to find the length of its longest
+ * harmonious subsequence among all its possible subsequences.
+ * 
+ * Example 1:
+ * 
+ * Input: [1,3,2,2,5,2,3,7]
+ * Output: 5
+ * Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+ * 
+ * 
+ * 
+ * Note:
+ * The length of the input array will not exceed 20,000.
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Long, Integer> map = new HashMap<>();
+        for (long num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int result = 0;
+        for (long key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                result = Math.max(result, map.get(key + 1) + map.get(key));
+            }
+        }
+        return result;        
+    }
+}
