6324a4c69006b65d96afdfec7ce995eafbb397e4
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-13 13:46:04 +0800

    add 441

---
+++ 441.arranging-coins.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=441 lang=java
+ *
+ * [441] Arranging Coins
+ *
+ * https://leetcode.com/problems/arranging-coins/description/
+ *
+ * algorithms
+ * Easy (37.35%)
+ * Total Accepted:    62.7K
+ * Total Submissions: 168K
+ * Testcase Example:  '5'
+ *
+ * You have a total of n coins that you want to form in a staircase shape,
+ * where every k-th row must have exactly k coins.
+ * ⁠
+ * Given n, find the total number of full staircase rows that can be formed.
+ * 
+ * n is a non-negative integer and fits within the range of a 32-bit signed
+ * integer.
+ * 
+ * Example 1:
+ * 
+ * n = 5
+ * 
+ * The coins can form the following rows:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤
+ * 
+ * Because the 3rd row is incomplete, we return 2.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * n = 8
+ * 
+ * The coins can form the following rows:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤ ¤
+ * ¤ ¤
+ * 
+ * Because the 4th row is incomplete, we return 3.
+ * 
+ * 
+ */
+class Solution {
+    public int arrangeCoins(int n) {
+        int i=0;
+        while(n > 0){
+            i+=1;
+            n-=i;
+        }
+        return n==0 ? i : i-1;
+    }
+}
