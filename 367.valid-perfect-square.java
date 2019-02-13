d04d65d75716ea51e9a61f2040ac1b2c2eee0c88
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-13 13:57:05 +0800

    add 367

---
+++ 367.valid-perfect-square.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=367 lang=java
+ *
+ * [367] Valid Perfect Square
+ *
+ * https://leetcode.com/problems/valid-perfect-square/description/
+ *
+ * algorithms
+ * Easy (39.30%)
+ * Total Accepted:    98.3K
+ * Total Submissions: 250K
+ * Testcase Example:  '16'
+ *
+ * Given a positive integer num, write a function which returns True if num is
+ * a perfect square else False.
+ * 
+ * Note: Do not use any built-in library function such as sqrt.
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * Input: 16
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 14
+ * Output: false
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+}
