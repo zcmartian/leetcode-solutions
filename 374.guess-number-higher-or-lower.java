85ca5e18d1f7cdb0cb830d00165017b45f20252c
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-13 13:17:19 +0800

    add 374 python

---
+++ 374.guess-number-higher-or-lower.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=374 lang=java
+ *
+ * [374] Guess Number Higher or Lower
+ *
+ * https://leetcode.com/problems/guess-number-higher-or-lower/description/
+ *
+ * algorithms
+ * Easy (38.56%)
+ * Total Accepted:    97.4K
+ * Total Submissions: 252.5K
+ * Testcase Example:  '10\n6'
+ *
+ * We are playing the Guess Game. The game is as follows:
+ * 
+ * I pick a number from 1 to n. You have to guess which number I picked.
+ * 
+ * Every time you guess wrong, I'll tell you whether the number is higher or
+ * lower.
+ * 
+ * You call a pre-defined API guess(int num) which returns 3 possible results
+ * (-1, 1, or 0):
+ * 
+ * 
+ * -1 : My number is lower
+ * ⁠1 : My number is higher
+ * ⁠0 : Congrats! You got it!
+ * 
+ * 
+ * Example :
+ * 
+ * 
+ * 
+ * Input: n = 10, pick = 6
+ * Output: 6
+ * 
+ * 
+ * 
+ */
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int i = 1, j = n;
+        while(i < j) {
+            int mid = i + (j - i) / 2;
+            if(guess(mid) == 0) {
+                return mid;
+            } else if(guess(mid) == 1) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
+        }
+        return i;
+    }
+}
