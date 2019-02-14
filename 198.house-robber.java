9af3af397512e5a4b708d5af31d1bcf018d073b3
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-14 15:57:20 +0800

    add memo in recursion, reduce unnessesary recursion

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -42,13 +42,21 @@
  * 
  */
 class Solution {
+    private int[] memo;
     public int rob(int[] nums) {
+        memo = new int[nums.length + 1];
+        Arrays.fill(memo, -1);
         return rob(nums, nums.length-1);
     }
     private int rob(int[] nums, int n) {
         if(n < 0) {
             return 0;
         }
-        return Math.max(rob(nums, n - 2) + nums[n], rob(nums, n - 1));
+        if(memo[n]>=0) {
+            return memo[n];
+        }
+        int result = Math.max(rob(nums, n - 2) + nums[n], rob(nums, n - 1));
+        memo[n] = result;
+        return result;
     }
 }
