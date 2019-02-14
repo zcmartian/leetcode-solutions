b5e2eaef416f532148fddd418620159e0a50b421
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-14 16:40:21 +0800

    add 303

---
+++ 303.range-sum-query-immutable.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=303 lang=java
+ *
+ * [303] Range Sum Query - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (36.33%)
+ * Total Accepted:    125.3K
+ * Total Submissions: 344.7K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * Given an integer array nums, find the sum of the elements between indices i
+ * and j (i ≤ j), inclusive.
+ * 
+ * Example:
+ * 
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ * 
+ * 
+ */
+class NumArray {
+    private int[] nums;
+    public NumArray(int[] nums) {
+        for(int i =1;i<nums.length;i++){
+            nums[i] += nums[i-1];
+        }
+        this.nums = nums;
+    }
+    
+    public int sumRange(int i, int j) {
+        if(i==0)
+            return nums[j];
+        return nums[j] - nums[i -1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
