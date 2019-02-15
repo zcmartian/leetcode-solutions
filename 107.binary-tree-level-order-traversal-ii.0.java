35ce6ac75d2bf78b53afdd806fbc764287a14d53
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-15 10:08:09 +0800

    add 107 recursion

---
+++ 107.binary-tree-level-order-traversal-ii.0.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=107 lang=java
+ *
+ * [107] Binary Tree Level Order Traversal II
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Easy (45.41%)
+ * Total Accepted:    206.3K
+ * Total Submissions: 454.4K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the bottom-up level order traversal of its
+ * nodes' values. (ie, from left to right, level by level from leaf to root).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its bottom-up level order traversal as:
+ * 
+ * [
+ * ⁠ [15,7],
+ * ⁠ [9,20],
+ * ⁠ [3]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        levelMaker(ret, root, 0);
+        return ret;
+    }
+    private void levelMaker(List<List<Integer>> ret, TreeNode node, int level) {
+        if(node == null)
+            return;
+        if(level >= ret.size())
+            ret.add(0, new LinkedList<Integer>());
+        levelMaker(ret, node.left, level+1);
+        levelMaker(ret, node.right, level+1);
+        ret.get(ret.size()-1-level).add(node.val);
+    }
+}
