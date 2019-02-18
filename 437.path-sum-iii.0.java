a6b4e0fff6f2a28be511bc3d54164c76648d1812
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-18 14:58:27 +0800

    add 437 using map

---
+++ 437.path-sum-iii.0.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=437 lang=java
+ *
+ * [437] Path Sum III
+ *
+ * https://leetcode.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Easy (41.81%)
+ * Total Accepted:    91.3K
+ * Total Submissions: 218.3K
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * You are given a binary tree in which each node contains an integer value.
+ * 
+ * Find the number of paths that sum to a given value.
+ * 
+ * The path does not need to start or end at the root or a leaf, but it must go
+ * downwards
+ * (traveling only from parent nodes to child nodes).
+ * 
+ * The tree has no more than 1,000 nodes and the values are in the range
+ * -1,000,000 to 1,000,000.
+ * 
+ * Example:
+ * 
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ * 
+ * ⁠     10
+ * ⁠    /  \
+ * ⁠   5   -3
+ * ⁠  / \    \
+ * ⁠ 3   2   11
+ * ⁠/ \   \
+ * 3  -2   1
+ * 
+ * Return 3. The paths that sum to 8 are:
+ * 
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3. -3 -> 11
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        helper(root, 0, sum, preSum);
+        return count;
+    }
+    int count = 0;
+    public void helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return;
+        }
+
+        currSum += root.val;
+
+        if (preSum.containsKey(currSum - target)) {
+            count += preSum.get(currSum - target);
+        }
+
+        if (!preSum.containsKey(currSum)) {
+            preSum.put(currSum, 1);
+        } else {
+            preSum.put(currSum, preSum.get(currSum)+1);
+        }
+
+        helper(root.left, currSum, target, preSum);
+        helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+    }
+}
