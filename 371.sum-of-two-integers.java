59a91e7fc92d648bdc12a31d89481112f39f3c8a
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-19 10:31:42 +0800

    add 371

---
+++ 371.sum-of-two-integers.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=371 lang=java
+ *
+ * [371] Sum of Two Integers
+ *
+ * https://leetcode.com/problems/sum-of-two-integers/description/
+ *
+ * algorithms
+ * Easy (51.31%)
+ * Total Accepted:    124.2K
+ * Total Submissions: 242K
+ * Testcase Example:  '1\n2'
+ *
+ * Calculate the sum of two integers a and b, but you are not allowed to use
+ * the operator + and -.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: a = 1, b = 2
+ * Output: 3
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: a = -2, b = 3
+ * Output: 1
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int getSum(int a, int b) {
+        int carry;
+        while(b != 0) {
+            carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
