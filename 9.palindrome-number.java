dfd748b39e34cce6bf63e3a045d8510f81a4ff9c
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-19 14:17:20 +0800

    add 9

---
+++ 9.palindrome-number.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (41.72%)
+ * Total Accepted:    503.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '121'
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome
+ * when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 121
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ * 
+ * 
+ * Follow up:
+ * 
+ * Coud you solve it without converting the integer to a string?
+ * 
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        List<String> s = new ArrayList<>();
+        while(x!=0) {
+            s.add(String.valueOf(x%10));
+            x /= 10;
+        }
+        for(int i=0,j=s.size()-1;i<j;) {
+            if(!s.get(i).equals(s.get(j)))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
