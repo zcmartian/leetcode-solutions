52017a491f859a3b582110d27cd8f783e06c9265
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-19 13:12:43 +0800

    add 171

---
+++ 171.excel-sheet-column-number.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=171 lang=java
+ *
+ * [171] Excel Sheet Column Number
+ *
+ * https://leetcode.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (50.76%)
+ * Total Accepted:    205.6K
+ * Total Submissions: 405.1K
+ * Testcase Example:  '"A"'
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding
+ * column number.
+ * 
+ * For example:
+ * 
+ * 
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28 
+ * ⁠   ...
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A"
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "AB"
+ * Output: 28
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "ZY"
+ * Output: 701
+ * 
+ */
+class Solution {
+    public int titleToNumber(String s) {
+        char[] cs = s.toCharArray();
+        int n = cs.length;
+        int ret = 0;
+        for(int i=0;i<n;i++) {
+            ret += (cs[i]-'A'+1) * Math.pow(26, n-i-1);
+        }
+        return ret;
+    }
+}
