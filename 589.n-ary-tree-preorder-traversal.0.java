3e215c95aaf0d05ee54340631ba3e1da40c1ccfb
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-20 11:42:17 +0800

    add 589 iteration

---
+++ 589.n-ary-tree-preorder-traversal.0.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=589 lang=java
+ *
+ * [589] N-ary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (65.77%)
+ * Total Accepted:    28.8K
+ * Total Submissions: 43.8K
+ * Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+ *
+ * Given an n-ary tree, return the preorder traversal of its nodes' values.
+ * 
+ * For example, given a 3-ary tree:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Return its preorder traversal as: [1,3,5,6,2,4].
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+
+        while (!stack.empty()) {
+            root = stack.pop();
+            list.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--)
+                stack.add(root.children.get(i));
+        }
+
+        return list;
+    }
+}
