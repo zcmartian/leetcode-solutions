ac6f697f6633ca428c2abd2697b7fd79e71c35f6
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-22 13:10:45 +0800

    add 429

---
+++ 429.n-ary-tree-level-order-traversal.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=429 lang=java
+ *
+ * [429] N-ary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/n-ary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Easy (58.14%)
+ * Total Accepted:    22.7K
+ * Total Submissions: 39K
+ * Testcase Example:  '{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}'
+ *
+ * Given an n-ary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * For example, given a 3-ary tree:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * We should return its level order traversal:
+ * 
+ * 
+ * [
+ * ⁠    [1],
+ * ⁠    [3,2,4],
+ * ⁠    [5,6]
+ * ]
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The depth of the tree is at most 1000.
+ * The total number of nodes is at most 5000.
+ * 
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if(root==null) return ret;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++) {
+                Node node = q.poll();
+                list.add(node.val);
+                for(Node c : node.children)
+                    q.offer(c);
+            }
+            ret.add(list);
+        }
+        return ret;
+    }
+}
