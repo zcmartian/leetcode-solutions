fdab7e4545c2e1814a56e56c0e3945bccb67fab9
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-25 17:47:12 +0800

    add 627 sql

---
+++ 627.swap-salary.sql
@@ -0,0 +1,44 @@
+--
+-- @lc app=leetcode id=627 lang=mysql
+--
+-- [627] Swap Salary
+--
+-- https://leetcode.com/problems/swap-salary/description/
+--
+-- database
+-- Easy (69.12%)
+-- Total Accepted:    55K
+-- Total Submissions: 79.7K
+-- Testcase Example:  '{"headers":{"salary":["id","name","sex","salary"]},"rows":{"salary":[[1,"A","m",2500],[2,"B","f",1500],[3,"C","m",5500],[4,"D","f",500]]}}'
+--
+-- Given a table salary, such as the one below, that has m=male and f=female
+-- values. Swap all f and m values (i.e., change all f values to m and vice
+-- versa) with a single update query and no intermediate temp table.
+-- 
+-- For example:
+-- 
+-- 
+-- 
+-- 
+-- | id | name | sex | salary |
+-- |----|------|-----|--------|
+-- | 1  | A    | m   | 2500   |
+-- | 2  | B    | f   | 1500   |
+-- | 3  | C    | m   | 5500   |
+-- | 4  | D    | f   | 500    |
+-- 
+-- After running your query, the above salary table should have the following
+-- rows:
+-- 
+-- 
+-- | id | name | sex | salary |
+-- |----|------|-----|--------|
+-- | 1  | A    | f   | 2500   |
+-- | 2  | B    | m   | 1500   |
+-- | 3  | C    | f   | 5500   |
+-- | 4  | D    | m   | 500    |
+-- 
+-- 
+--
+# Write your MySQL query statement below
+SELECT ID,NAME,CASE WHEN SEX='m' THEN 'f' ELSE 'm' END SEX, SALARY FROM SALARY;
