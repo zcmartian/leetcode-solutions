705cc52c5b440cd8a7272fcc6c728c2790b4779c
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-25 15:12:42 +0800

    add 844

---
+++ 844.backspace-string-compare.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=844 lang=java
+ *
+ * [844] Backspace String Compare
+ *
+ * https://leetcode.com/problems/backspace-string-compare/description/
+ *
+ * algorithms
+ * Easy (45.14%)
+ * Total Accepted:    43.6K
+ * Total Submissions: 96.7K
+ * Testcase Example:  '"ab#c"\n"ad#c"'
+ *
+ * Given two strings S and T, return if they are equal when both are typed into
+ * empty text editors. # means a backspace character.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: S = "ab#c", T = "ad#c"
+ * Output: true
+ * Explanation: Both S and T become "ac".
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: S = "ab##", T = "c#d#"
+ * Output: true
+ * Explanation: Both S and T become "".
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: S = "a##c", T = "#a#c"
+ * Output: true
+ * Explanation: Both S and T become "c".
+ * 
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: S = "a#c", T = "b"
+ * Output: false
+ * Explanation: S becomes "c" while T becomes "b".
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= S.length <= 200
+ * 1 <= T.length <= 200
+ * S and T only contain lowercase letters and '#' characters.
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * Can you solve it in O(N) time and O(1) space?
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ *  1. Thoughts
+ *      - from back to front
+ *      - if count > 0 && != #, skip
+ *      - if count == 0 && different, return false
+ *      - if #, count++
+ *      - if one ends, the other count == 0 && has any char return false
+ *      - else return true
+*/
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length()-1;
+        int j = T.length()-1;
+        int countS = 0;
+        int countT = 0;
+        while (i >= 0 || j >= 0) {
+            while (i >= 0 && (countS > 0 || S.charAt(i) == '#')) {
+                if (S.charAt(i) == '#') countS++;
+                else countS--;
+                i--;
+            }
+            char left = i < 0 ? '@' : S.charAt(i);
+            while (j >= 0 && (countT > 0 || T.charAt(j) == '#')) {
+                if (T.charAt(j) == '#') countT++;
+                else countT--;
+                j--;
+            }
+            char right = j < 0 ? '@' : T.charAt(j);
+            if (left != right) return false;
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
