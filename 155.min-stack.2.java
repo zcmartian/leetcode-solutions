032569e11c624dd1ae201ac79cc21634084c30d4
zhouchao3 <zhouchao3@xiaomi.com>
2019-02-28 14:53:25 +0800

    add 155 forth

---
+++ 155.min-stack.2.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=155 lang=java
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (35.34%)
+ * Total Accepted:    263.1K
+ * Total Submissions: 744.4K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+class MinStack {
+    private Stack<Integer> data;
+    private Stack<Integer> min;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        data = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int x) {
+        data.push(x);
+        if(min.isEmpty() || getMin() > x)
+            min.push(x);
+        else {
+            min.push(getMin());
+        }
+    }
+    
+    public void pop() {
+        if(data.isEmpty())
+            throw new RuntimeException("stack empty");
+        data.pop();
+        min.pop();
+    }
+    
+    public int top() {
+        if(data.isEmpty())
+            throw new RuntimeException("stack empty");
+        return data.peek();
+    }
+    
+    public int getMin() {
+        if(min.isEmpty())
+            throw new RuntimeException("stack empty");
+        return min.peek();
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
