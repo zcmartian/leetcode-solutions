966a3aecc04a2b2692a7f7f4bc8801564ad0d37e
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-01 13:40:23 +0800

    add 41

---
+++ 41.first-missing-positive.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=41 lang=java
+ *
+ * [41] First Missing Positive
+ *
+ * https://leetcode.com/problems/first-missing-positive/description/
+ *
+ * algorithms
+ * Hard (28.20%)
+ * Total Accepted:    191.5K
+ * Total Submissions: 679K
+ * Testcase Example:  '[1,2,0]'
+ *
+ * Given an unsorted integer array, find the smallest missing positive
+ * integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,0]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,-1,1]
+ * Output: 2
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,8,9,11,12]
+ * Output: 1
+ * 
+ * 
+ * Note:
+ * 
+ * Your algorithm should run in O(n) time and uses constant extra space.
+ * 
+ */
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int i = 0, n = nums.length;
+        while (i < n) {
+            // If the current value is in the range of (0,length) and it's not at its correct position,
+            // swap it to its correct position.
+            // Else just continue;
+            if (nums[i] >= 0 && nums[i] < n && nums[nums[i]] != nums[i])
+                swap(nums, i, nums[i]);
+            else
+                i++;
+        }
+        int k = 1;
+
+        // Check from k=1 to see whether each index and value can be corresponding.
+        while (k < n && nums[k] == k)
+            k++;
+
+        // If it breaks because of empty array or reaching the end. K must be the first missing number.
+        if (n == 0 || k < n)
+            return k;
+        else   // If k is hiding at position 0, K+1 is the number.
+            return nums[0] == k ? k + 1 : k;
+
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
