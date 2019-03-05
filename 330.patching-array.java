0ff2f899d98c144f9ded218baf954602bf540f34
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-05 09:52:57 +0800

    add 330 second

--- 330.patching-array.java
+++ 330.patching-array.java
@@ -46,7 +46,9 @@
  */
 class Solution {
     public int minPatches(int[] nums, int n) {
-        long miss = 1, added = 0, i = 0;
+        long miss = 1;
+        int added = 0;
+        int i = 0;
         while (miss <= n) {
             if (i < nums.length && nums[i] <= miss) {
                 miss += nums[i++];
