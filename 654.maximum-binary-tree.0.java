d68042fc201ba9943524e5ac9220f979b5be87e0
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-05 16:57:45 +0800

    add 654 stack

---
+++ 654.maximum-binary-tree.0.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=654 lang=java
+ *
+ * [654] Maximum Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-binary-tree/description/
+ *
+ * algorithms
+ * Medium (75.08%)
+ * Total Accepted:    71.2K
+ * Total Submissions: 94.9K
+ * Testcase Example:  '[3,2,1,6,0,5]'
+ *
+ * 
+ * Given an integer array with no duplicates. A maximum tree building on this
+ * array is defined as follow:
+ * 
+ * The root is the maximum number in the array. 
+ * The left subtree is the maximum tree constructed from left part subarray
+ * divided by the maximum number.
+ * The right subtree is the maximum tree constructed from right part subarray
+ * divided by the maximum number. 
+ * 
+ * 
+ * 
+ * 
+ * Construct the maximum tree by the given array and output the root node of
+ * this tree.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: [3,2,1,6,0,5]
+ * Output: return the tree root node representing the following tree:
+ * 
+ * ⁠     6
+ * ⁠   /   \
+ * ⁠  3     5
+ * ⁠   \    / 
+ * ⁠    2  0   
+ * ⁠      \
+ * ⁠       1
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The size of the given array will be in the range [1,1000].
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
+                curr.left = stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                stack.peek().right = curr;
+            }
+            stack.push(curr);
+        }
+
+        return stack.isEmpty() ? null : stack.removeLast();
+    }
+}
