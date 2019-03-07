6dc66568ceec7bfd6cdeb71d0a3eaabbcb692495
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-07 10:26:57 +0800

    add 208

---
+++ 208.implement-trie-prefix-tree.java
@@ -0,0 +1,113 @@
+/*
+ * @lc app=leetcode id=208 lang=java
+ *
+ * [208] Implement Trie (Prefix Tree)
+ *
+ * https://leetcode.com/problems/implement-trie-prefix-tree/description/
+ *
+ * algorithms
+ * Medium (36.84%)
+ * Total Accepted:    163.2K
+ * Total Submissions: 442.9K
+ * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+ *
+ * Implement a trie with insert, search, and startsWith methods.
+ * 
+ * Example:
+ * 
+ * 
+ * Trie trie = new Trie();
+ * 
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");   
+ * trie.search("app");     // returns true
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
+ * 
+ * 
+ */
+class Trie {
+
+    private Node root;
+    private int size;
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node(cur));
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+        }    
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+        public Node parent;
+
+        public Node(boolean isWord, Node parent) {
+            this.isWord = isWord;
+            this.parent = parent;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(null);
+        }
+
+        public Node(Node parent) {
+            this(false, parent);
+        }
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
