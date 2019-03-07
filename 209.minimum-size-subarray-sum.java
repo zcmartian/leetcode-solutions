289e2080ae830df0c86fe207a9ec145d74bb541e
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-07 10:44:13 +0800

    add 209 On

---
+++ 209.minimum-size-subarray-sum.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=209 lang=java
+ *
+ * [209] Minimum Size Subarray Sum
+ *
+ * https://leetcode.com/problems/minimum-size-subarray-sum/description/
+ *
+ * algorithms
+ * Medium (34.28%)
+ * Total Accepted:    163.6K
+ * Total Submissions: 477.3K
+ * Testcase Example:  '7\n[2,3,1,2,4,3]'
+ *
+ * Given an array of n positive integers and a positive integer s, find the
+ * minimal length of a contiguous subarray of which the sum ≥ s. If there isn't
+ * one, return 0 instead.
+ * 
+ * Example: 
+ * 
+ * 
+ * Input: s = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: the subarray [4,3] has the minimal length under the problem
+ * constraint.
+ * 
+ * Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution of
+ * which the time complexity is O(n log n). 
+ * 
+ */
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int start = 0, end = 0, sum = 0, minLen = Integer.MAX_VALUE;
+        while (end < nums.length) {
+            while (end < nums.length && sum < s) sum += nums[end++];
+            if (sum < s) break;
+            while (start < end && sum >= s) sum -= nums[start++];
+            if (end - start + 1 < minLen) minLen = end - start + 1;
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;        
+    }
+}
