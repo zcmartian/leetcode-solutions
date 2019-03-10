c16ea977b5c10fac32d3711a0ad3dc47d055a01c
zcmartian <zcmit@126.com>
2019-03-10 15:23:52 +0800

    add 49

---
+++ 49.group-anagrams.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=49 lang=java
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (44.94%)
+ * Total Accepted:    300.6K
+ * Total Submissions: 668.8K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings, group anagrams together.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ * 
+ * Note:
+ * 
+ * 
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ * 
+ * 
+ */
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};//最多10609个z
+
+        List<List<String>> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (String s : strs) {
+            int key = 1;
+            for (char c : s.toCharArray()) {
+                key *= prime[c - 'a'];
+            }
+            List<String> t;
+            if (map.containsKey(key)) {
+                t = res.get(map.get(key));
+            } else {
+                t = new ArrayList<>();
+                res.add(t);
+                map.put(key, res.size() - 1);
+            }
+            t.add(s);
+        }
+        return res;
+    }
+}
