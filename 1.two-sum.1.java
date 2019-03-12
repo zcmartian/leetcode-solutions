9bca1f66bb3fbbc258c0509ef3a0a1ea85ac5252
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-12 17:58:29 +0800

    add 1 third

---
+++ 1.two-sum.1.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (39.90%)
+ * Total Accepted:    1.3M
+ * Total Submissions: 3.4M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            int complete = target - nums[i];
+            if(map.containsKey(complete))
+                return new int[] {map.get(complete), i};
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
