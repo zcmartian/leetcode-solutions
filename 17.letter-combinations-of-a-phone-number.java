fba5bd283ac3169982878cc6909d3bf8832f19d2
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-14 14:31:23 +0800

    add 17

---
+++ 17.letter-combinations-of-a-phone-number.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=17 lang=java
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (40.58%)
+ * Total Accepted:    354.2K
+ * Total Submissions: 872.9K
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * 
+ * Note:
+ * 
+ * Although the above answer is in lexicographical order, your answer could be
+ * in any order you want.
+ * 
+ */
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        if(digits.isEmpty()) return ans;
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        while(ans.peek().length()!=digits.length()){
+            String remove = ans.remove();
+            String map = mapping[digits.charAt(remove.length())-'0'];
+            for(char c: map.toCharArray()){
+                ans.addLast(remove+c);
+            }
+        }
+        return ans;
+    }
+}
