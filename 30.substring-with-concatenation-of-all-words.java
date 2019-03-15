a795f110782af6b8f1a8c6c5ce36dcd3f2eae727
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-15 14:40:16 +0800

    add 30

---
+++ 30.substring-with-concatenation-of-all-words.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=30 lang=java
+ *
+ * [30] Substring with Concatenation of All Words
+ *
+ * https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
+ *
+ * algorithms
+ * Hard (23.20%)
+ * Total Accepted:    124.2K
+ * Total Submissions: 535.4K
+ * Testcase Example:  '"barfoothefoobarman"\n["foo","bar"]'
+ *
+ * You are given a string, s, and a list of words, words, that are all of the
+ * same length. Find all starting indices of substring(s) in s that is a
+ * concatenation of each word in words exactly once and without any intervening
+ * characters.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * ⁠ s = "barfoothefoobarman",
+ * ⁠ words = ["foo","bar"]
+ * Output: [0,9]
+ * Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar"
+ * respectively.
+ * The output order does not matter, returning [9,0] is fine too.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * ⁠ s = "wordgoodgoodgoodbestword",
+ * ⁠ words = ["word","good","best","word"]
+ * Output: []
+ * 
+ * 
+ */
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> indexes = new ArrayList<>();
+        if(s==null||s.length()==0||words==null||words.length==0) return indexes;
+        Map<String, Integer> counts = new HashMap<>();
+        for (final String word : words) {
+            counts.put(word, counts.getOrDefault(word, 0) + 1);
+        }
+        int n = s.length(), num = words.length, len = words[0].length();
+        for (int i = 0; i < n - num * len + 1; i++) {
+            Map<String, Integer> seen = new HashMap<>();
+            int j = 0;
+            while (j < num) {
+                String word = s.substring(i + j * len, i + (j + 1) * len);
+                if (counts.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    if (seen.get(word) > counts.getOrDefault(word, 0)) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+                j++;
+            }
+            if (j == num) {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+}
