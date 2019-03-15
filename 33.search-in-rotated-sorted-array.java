7154f6fe1d63651a22bf6d8ff774c022aceff990
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-15 15:53:24 +0800

    add 33

---
+++ 33.search-in-rotated-sorted-array.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (32.68%)
+ * Total Accepted:    377.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * 
+ */
+class Solution {
+    public int search(int[] A, int target) {
+        if(A==null||A.length==0) return -1;
+        int lo = 0;
+        int hi = A.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (A[mid] == target) return mid;
+
+            if (A[lo] <= A[mid]) {
+                if (target >= A[lo] && target < A[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > A[mid] && target <= A[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return A[lo] == target ? lo : -1;
+    }
+
+}
