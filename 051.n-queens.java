b25e67953169db51f0ca51f789e85b872075d18f
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-18 11:44:24 +0800

    add 51

---
+++ 051.n-queens.java
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=51 lang=java
+ *
+ * [51] N-Queens
+ *
+ * https://leetcode.com/problems/n-queens/description/
+ *
+ * algorithms
+ * Hard (37.99%)
+ * Total Accepted:    131.6K
+ * Total Submissions: 346.5K
+ * Testcase Example:  '4'
+ *
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard
+ * such that no two queens attack each other.
+ * 
+ * 
+ * 
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * 
+ * Each solution contains a distinct board configuration of the n-queens'
+ * placement, where 'Q' and '.' both indicate a queen and an empty space
+ * respectively.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 4
+ * Output: [
+ * ⁠[".Q..",  // Solution 1
+ * ⁠ "...Q",
+ * ⁠ "Q...",
+ * ⁠ "..Q."],
+ * 
+ * ⁠["..Q.",  // Solution 2
+ * ⁠ "Q...",
+ * ⁠ "...Q",
+ * ⁠ ".Q.."]
+ * ]
+ * Explanation: There exist two distinct solutions to the 4-queens puzzle as
+ * shown above.
+ * 
+ * 
+ */
+class Solution {
+    private boolean[] col;
+    private boolean[] dia1;
+    private boolean[] dia2;
+    private ArrayList<List<String>> res;
+
+    public List<List<String>> solveNQueens(int n) {
+
+        res = new ArrayList<List<String>>();
+        col = new boolean[n];
+        dia1 = new boolean[2 * n - 1];
+        dia2 = new boolean[2 * n - 1];
+
+        LinkedList<Integer> row = new LinkedList<Integer>();
+        putQueen(n, 0, row);
+
+        return res;
+    }
+
+    private void putQueen(int n, int index, LinkedList<Integer> row){
+
+        if(index == n){
+            res.add(generateBoard(n, row));
+            return;
+        }
+
+        for(int i = 0 ; i < n ; i ++)
+            if(!col[i] && !dia1[index + i] && !dia2[index - i + n - 1]){
+                row.addLast(i);
+                col[i] = true;
+                dia1[index + i] = true;
+                dia2[index - i + n - 1] = true;
+                putQueen(n, index + 1, row);
+                col[i] = false;
+                dia1[index + i] = false;
+                dia2[index - i + n - 1] = false;
+                row.removeLast();
+            }
+
+        return;
+    }
+
+    private List<String> generateBoard(int n, LinkedList<Integer> row){
+
+        assert row.size() == n;
+
+        ArrayList<String> board = new ArrayList<String>();
+        for(int i = 0 ; i < n ; i ++){
+            char[] charArray = new char[n];
+            Arrays.fill(charArray, '.');
+            charArray[row.get(i)] = 'Q';
+            board.add(new String(charArray));
+        }
+        return board;
+    }
+}
