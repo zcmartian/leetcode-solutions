8e579c7b697c5f2d1b68219e34e3ce00f02718cc
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-20 16:15:28 +0800

    add 60 Time Limit Exceeded

---
+++ 060.permutation-sequence__Time-Limit-Exceeded.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=60 lang=java
+ *
+ * [60] Permutation Sequence
+ *
+ * https://leetcode.com/problems/permutation-sequence/description/
+ *
+ * algorithms
+ * Medium (32.48%)
+ * Total Accepted:    131.2K
+ * Total Submissions: 403.8K
+ * Testcase Example:  '3\n3'
+ *
+ * The set [1,2,3,...,n] contains a total of n! unique permutations.
+ * 
+ * By listing and labeling all of the permutations in order, we get the
+ * following sequence for n = 3:
+ * 
+ * 
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 
+ * 
+ * Given n and k, return the k^th permutation sequence.
+ * 
+ * Note:
+ * 
+ * 
+ * Given n will be between 1 and 9 inclusive.
+ * Given k will be between 1 and n! inclusive.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 3, k = 3
+ * Output: "213"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 4, k = 9
+ * Output: "2314"
+ * 
+ * 
+ */
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<List<Integer>> list = new ArrayList<>();
+        int[] nums = new int[n];
+        for(int i=1;i<=n;i++) {
+            nums[i-1] = i;
+        }
+        backtrack(list, new ArrayList<Integer>(), nums);
+        List<Integer> cur = list.get(k-1);
+        StringBuilder sb = new StringBuilder();
+        for(int i : cur) {
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(tempList.contains(nums[i])) continue; // element already exists, skip
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
