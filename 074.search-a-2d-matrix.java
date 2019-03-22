a571fd6e0e8b4177db59e203dcfb24734a9dcc29
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-22 14:34:46 +0800

    add 74

---
+++ 074.search-a-2d-matrix.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=74 lang=java
+ *
+ * [74] Search a 2D Matrix
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix/description/
+ *
+ * algorithms
+ * Medium (34.70%)
+ * Total Accepted:    210.9K
+ * Total Submissions: 607.9K
+ * Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,50]]\n3'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the
+ * previous row.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * matrix = [
+ * ⁠ [1,   3,  5,  7],
+ * ⁠ [10, 11, 16, 20],
+ * ⁠ [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * Output: false
+ * 
+ */
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null||matrix.length==0) return false;
+
+        int i = 0, j = matrix[0].length-1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
