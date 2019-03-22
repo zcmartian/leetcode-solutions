fc9f064aef4231ecb56cd7566bacca9fa6d6bbd7
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-22 15:20:25 +0800

    add comment

--- 076.minimum-window-substring.java
+++ 076.minimum-window-substring.java
@@ -32,29 +32,31 @@
  */
 class Solution {
     public String minWindow(String s, String t) {
-        int[] arr = new int[128];
+        int[] arr = new int[128];// 用一个map存储匹配字符串中字符的个数
         for (int i = 0; i < t.length(); i++) {
             arr[t.charAt(i)]++;
         }
 
         int counter = t.length(), left = 0, right = 0, window = Integer.MAX_VALUE, head = -1;
-        while (right < s.length()) {
+        while (right < s.length()) { //将有指针往后扫
             char rc = s.charAt(right++);
-            if (arr[rc] > 0) {
+            if (arr[rc] > 0) {//遇到map有的相应的字符计数减一
                 counter--;
             }
-            arr[rc]--;
+            arr[rc]--;//同时,待匹配字符串每个字符位个数减一,这样没有在匹配串中的个数为负数,匹配的为0
 
-            while (counter == 0) {
-                if (right - left < window) {
+            while (counter == 0) {//扫描完了匹配串
+                if (right - left < window) {//找一个更短的待匹配串
                     window = right - (head = left);
                 }
 
-                char lc = s.charAt(left++);
-                if (arr[lc] == 0) {
+                char lc = s.charAt(left++);//左指针往右扫,试图降低窗口大小
+                if (arr[lc] == 0) {//如果移除窗口的字符是匹配字符,则当前窗口已经不满足条件
+                    //则counter++意为跳出while循环重新在外层循环中右指针继续扫
                     counter++;
                 }
-                arr[lc]++;
+                arr[lc]++;//每个移出窗口的字符个数加一,此处对应46行的减一操作
+                //当初外层while循环曾将这个字符个数减一,现在加回去表示我已经考察过字符了,还原现场
             }
         }
         return head == -1 ? "" : s.substring(head, head + window);
