ba90d80744f3a75a9eaac6236f333213743a346e
zcmartian <zcmit@126.com>
2019-03-24 10:38:44 +0800

    add 90

---
+++ 090.subsets-ii.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=90 lang=java
+ *
+ * [90] Subsets II
+ *
+ * https://leetcode.com/problems/subsets-ii/description/
+ *
+ * algorithms
+ * Medium (41.68%)
+ * Total Accepted:    192.3K
+ * Total Submissions: 461.5K
+ * Testcase Example:  '[1,2,2]'
+ *
+ * Given a collection of integers that might contain duplicates, nums, return
+ * all possible subsets (the power set).
+ * 
+ * Note: The solution set must not contain duplicate subsets.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,2]
+ * Output:
+ * [
+ * ⁠ [2],
+ * ⁠ [1],
+ * ⁠ [1,2,2],
+ * ⁠ [2,2],
+ * ⁠ [1,2],
+ * ⁠ []
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
+
