bc8d910c9862328a8bdef118b7cf562fad21f704
zcmartian <zcmit@126.com>
2019-03-24 16:23:48 +0800

    add 93 recursion

---
+++ 093.restore-ip-addresses.0.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=93 lang=java
+ *
+ * [93] Restore IP Addresses
+ *
+ * https://leetcode.com/problems/restore-ip-addresses/description/
+ *
+ * algorithms
+ * Medium (30.90%)
+ * Total Accepted:    132.5K
+ * Total Submissions: 428.9K
+ * Testcase Example:  '"25525511135"'
+ *
+ * Given a string containing only digits, restore it by returning all possible
+ * valid IP address combinations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ * 
+ * 
+ */
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> solutions = new ArrayList<String>();
+        restoreIp(s, solutions, 0, "", 0);
+        return solutions;
+    }
+
+    private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+        if (count > 4) return;
+        if (count == 4 && idx == ip.length()) solutions.add(restored);
+
+        for (int i=1; i<4; i++) {
+            if (idx+i > ip.length()) break;
+            String s = ip.substring(idx,idx+i);
+            if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
+            restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+        }
+    }
+}
