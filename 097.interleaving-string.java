47c4cbf3ca9aefea96df511d47b485838593efa1
zcmartian <zcmit@126.com>
2019-03-24 17:02:50 +0800

    add 97

---
+++ 097.interleaving-string.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=97 lang=java
+ *
+ * [97] Interleaving String
+ *
+ * https://leetcode.com/problems/interleaving-string/description/
+ *
+ * algorithms
+ * Hard (27.47%)
+ * Total Accepted:    106.2K
+ * Total Submissions: 386.4K
+ * Testcase Example:  '"aabcc"\n"dbbca"\n"aadbbcbcac"'
+ *
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and
+ * s2.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * Output: false
+ * 
+ * 
+ */
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
+        int m = s1.length(), n = s2.length();
+        if(m + n != s3.length()) return false;
+        return dfs(c1, c2, c3, 0, 0, 0, new boolean[m + 1][n + 1]);
+    }
+
+    public boolean dfs(char[] c1, char[] c2, char[] c3, int i, int j, int k, boolean[][] invalid) {
+        if(invalid[i][j]) return false;
+        if(k == c3.length) return true;
+        boolean valid =
+            i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, invalid) ||
+            j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, invalid);
+        if(!valid) invalid[i][j] = true;
+        return valid;
+    }
+}
