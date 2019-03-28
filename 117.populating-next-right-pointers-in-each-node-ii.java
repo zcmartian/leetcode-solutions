5be1115ec8d1bcbd1ae67edaa8d09f071ff466cf
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-28 11:02:15 +0800

    add 117

---
+++ 117.populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=117 lang=java
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * algorithms
+ * Medium (33.76%)
+ * Total Accepted:    174K
+ * Total Submissions: 515.2K
+ * Testcase Example:  '{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}'
+ *
+ * Given a binary tree
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ * 
+ * Output:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+ * 
+ * Explanation: Given the above binary tree (Figure A), your function should
+ * populate each next pointer to point to its next right node, just like in
+ * Figure B.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You may only use constant extra space.
+ * Recursive approach is fine, implicit stack space does not count as extra
+ * space for this problem.
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        
+        if(root==null)
+            return root;
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+            int n=queue.size();
+            for(int i=0;i<n;++i)
+            {
+                Node temp=queue.poll();
+                if(temp.left!=null)
+                    queue.add(temp.left);
+                if(temp.right!=null)
+                    queue.add(temp.right);
+                Node temp1;
+                if(i==n-1)
+                    temp1=null;
+                else
+                    temp1=queue.peek();
+                temp.next=temp1;
+            }
+        }
+        return root;
+    }
+}
