2f6d03397897291f9321fc50c6e5d1f2299125c0
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-29 10:30:13 +0800

    add 132

---
+++ 132.palindrome-partitioning-ii.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=132 lang=java
+ *
+ * [132] Palindrome Partitioning II
+ *
+ * https://leetcode.com/problems/palindrome-partitioning-ii/description/
+ *
+ * algorithms
+ * Hard (26.78%)
+ * Total Accepted:    97.8K
+ * Total Submissions: 365K
+ * Testcase Example:  '"aab"'
+ *
+ * Given a string s, partition s such that every substring of the partition is
+ * a palindrome.
+ * 
+ * Return the minimum cuts needed for a palindrome partitioning of s.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "aab"
+ * Output: 1
+ * Explanation: The palindrome partitioning ["aa","b"] could be produced using
+ * 1 cut.
+ * 
+ * 
+ */
+class Solution {
+    public int minCut(String s) {
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int[] cut = new int[n];
+        boolean[][] pal = new boolean[n][n];
+
+        for(int i = 0; i < n; i++) {
+            int min = i;
+            for(int j = 0; j <= i; j++) {
+                if(c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                    pal[j][i] = true;
+                    min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
+                }
+            }
+            cut[i] = min;
+        }
+        return cut[n - 1];
+    }
+}
