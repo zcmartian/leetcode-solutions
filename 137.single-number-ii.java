bcc33dc507ed47ea444588b4e8ea97407f43fdfa
zcmartian <zcmit@126.com>
2019-03-29 20:47:09 +0800

    add 137

---
+++ 137.single-number-ii.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=137 lang=java
+ *
+ * [137] Single Number II
+ *
+ * https://leetcode.com/problems/single-number-ii/description/
+ *
+ * algorithms
+ * Medium (45.41%)
+ * Total Accepted:    160.8K
+ * Total Submissions: 354K
+ * Testcase Example:  '[2,2,3,2]'
+ *
+ * Given a non-empty array of integers, every element appears three times
+ * except for one, which appears exactly once. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,3,2]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [0,1,0,1,0,1,99]
+ * Output: 99
+ *
+ * https://leetcode.com/problems/single-number-ii/discuss/43295/Detailed-explanation-and-generalization-of-the-bitwise-operation-method-for-single-numbers
+ * 
+ */
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x1 = 0, x2 = 0, mask = 0;
+         
+        for (int i : nums) {
+            x2 ^= x1 & i;
+            x1 ^= i;
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
+        }
+
+        return x1;  // Since p = 1, in binary form p = '01', then p1 = 1, so we should return x1. 
+                    // If p = 2, in binary form p = '10', then p2 = 1, and we should return x2.
+                    // Or alternatively we can simply return (x1 | x2).
+    }
+}
