2126e1cbd38dede32d3a7caa441467fb0e847918
zcmartian <zcmit@126.com>
2019-03-29 20:58:00 +0800

    add 138

---
+++ 138.copy-list-with-random-pointer.java
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ *
+ * https://leetcode.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (26.19%)
+ * Total Accepted:    231.1K
+ * Total Submissions: 882.5K
+ * Testcase Example:  '{"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}'
+ *
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ * 
+ * Return a deep copy of the list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * 
+ * {"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+ * 
+ * Explanation:
+ * Node 1's value is 1, both of its next and random pointer points to Node 2.
+ * Node 2's value is 2, its next pointer points to null and its random pointer
+ * points to itself.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You must return the copy of the given head as a reference to the cloned
+ * list.
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Map<Node, Node> map = new HashMap<>();
+
+        // loop 1. copy all the nodes
+        Node node = head;
+        while (node != null) {
+            map.put(node, new Node(node.val));
+            node = node.next;
+        }
+
+        // loop 2. assign next and random pointers
+        node = head;
+        while (node != null) {
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+
+        return map.get(head);
+    }
+}
