1d9157101009f429153c77c66d3754d4558cc488
zcmartian <zcmit@126.com>
2019-03-29 22:58:34 +0800

    add 142

---
+++ 142.linked-list-cycle-ii.java
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (31.29%)
+ * Total Accepted:    202.3K
+ * Total Submissions: 646.4K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * Note: Do not modify the linked list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * Can you solve it without using extra space?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null||head.next==null) return null;
+
+        ListNode first = head;
+        ListNode second = head;
+        boolean cycle = false;
+        while(first != null && second != null) {
+            first = first.next;
+            if(second.next == null) return null;
+            second = second.next.next;
+            if(first==second) {
+                cycle = true;
+                break;
+            }
+        }
+        if(!cycle) return null;
+        first = head;
+        while(first != second) {
+            first = first.next;
+            second = second.next;
+        }
+        return first;
+    }
+}
