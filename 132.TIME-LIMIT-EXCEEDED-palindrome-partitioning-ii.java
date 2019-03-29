3a3bf4c12faf760b027da89f06ccc08b5373466e
zhouchao3 <zhouchao3@xiaomi.com>
2019-03-29 10:23:37 +0800

    add 132 TLE

---
+++ 132.TIME-LIMIT-EXCEEDED-palindrome-partitioning-ii.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=132 lang=java
+ *
+ * [132] Palindrome Partitioning II
+ *
+ * https://leetcode.com/problems/palindrome-partitioning-ii/description/
+ *
+ * algorithms
+ * Hard (26.78%)
+ * Total Accepted:    97.8K
+ * Total Submissions: 365K
+ * Testcase Example:  '"aab"'
+ *
+ * Given a string s, partition s such that every substring of the partition is
+ * a palindrome.
+ * 
+ * Return the minimum cuts needed for a palindrome partitioning of s.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "aab"
+ * Output: 1
+ * Explanation: The palindrome partitioning ["aa","b"] could be produced using
+ * 1 cut.
+ * 
+ * 
+ */
+class Solution {
+    public int minCut(String s) {
+        List<List<String>> ret = partition(s);
+        int min = Integer.MAX_VALUE;
+        for(List<String> list : ret) {
+            min = Math.min(min, list.size());
+        }
+        return min - 1;
+    }
+    List<List<String>> resultLst;
+    ArrayList<String> currLst;
+    public List<List<String>> partition(String s) {
+        resultLst = new ArrayList<List<String>>();
+        currLst = new ArrayList<String>();
+        backTrack(s,0);
+        return resultLst;
+    }
+    public void backTrack(String s, int l){
+        if(currLst.size()>0 //the initial str could be palindrome
+                && l>=s.length()){
+            List<String> r = (ArrayList<String>) currLst.clone();
+            resultLst.add(r);
+                }
+        for(int i=l;i<s.length();i++){
+            if(isPalindrome(s,l,i)){
+                if(l==i)
+                    currLst.add(Character.toString(s.charAt(i)));
+                else
+                    currLst.add(s.substring(l,i+1));
+                backTrack(s,i+1);
+                currLst.remove(currLst.size()-1);
+            }
+        }
+    }
+    public boolean isPalindrome(String str, int l, int r){
+        if(l==r) return true;
+        while(l<r){
+            if(str.charAt(l)!=str.charAt(r)) return false;
+            l++;r--;
+        }
+        return true;
+    }
+}
