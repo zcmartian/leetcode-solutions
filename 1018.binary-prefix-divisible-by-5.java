a006915c3949a9d67ff6b7f427a6155e02e22c5f
zcmartian <zcmit@126.com>
2019-03-31 20:19:18 +0800

    add 1018

---
+++ 1018.binary-prefix-divisible-by-5.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=1018 lang=java
+ *
+ * [1018] Binary Prefix Divisible By 5
+ *
+ * https://leetcode.com/problems/binary-prefix-divisible-by-5/description/
+ *
+ * algorithms
+ * Easy (40.00%)
+ * Total Accepted:    3.2K
+ * Total Submissions: 8K
+ * Testcase Example:  '[0,1,1]'
+ *
+ * Given an array A of 0s and 1s, consider N_i: the i-th subarray from A[0] to
+ * A[i] interpreted as a binary number (from most-significant-bit to
+ * least-significant-bit.)
+ * 
+ * Return a list of booleans answer, where answer[i] is true if and only if N_i
+ * is divisible by 5.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [0,1,1]
+ * Output: [true,false,false]
+ * Explanation: 
+ * The input numbers in binary are 0, 01, 011; which are 0, 1, and 3 in
+ * base-10.  Only the first number is divisible by 5, so answer[0] is true.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,1,1]
+ * Output: [false,false,false]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [0,1,1,1,1,1]
+ * Output: [true,false,false,false,true,false]
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [1,1,1,0,1]
+ * Output: [false,false,false,false,false]
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= A.length <= 30000
+ * A[i] is 0 or 1
+ * 
+ */
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] A) {
+        List<Boolean> list = new ArrayList<>();
+        int remainder = 0;
+        for(int bit : A) {
+            if (bit == 1)
+                remainder = (remainder * 2 + 1) % 5;
+            if (bit == 0)
+                remainder = (remainder * 2) % 5;
+            if(remainder%5==0) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
