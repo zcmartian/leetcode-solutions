9aa95f5fccaa1ee69f9c6108c516a0632fb55a76
zcmartian <zcmit@126.com>
2019-03-31 11:53:38 +0800

    add 173

---
+++ 173.binary-search-tree-iterator.java
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=173 lang=java
+ *
+ * [173] Binary Search Tree Iterator
+ *
+ * https://leetcode.com/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (47.66%)
+ * Total Accepted:    193.3K
+ * Total Submissions: 405.5K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n[[[7,3,15,null,null,9,20]],[null],[null],[null],[null],[null],[null],[null],[null],[null]]'
+ *
+ * Implement an iterator over a binary search tree (BST). Your iterator will be
+ * initialized with the root node of a BST.
+ * 
+ * Calling next() will return the next smallest number in the BST.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * 
+ * 
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // return 3
+ * iterator.next();    // return 7
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 9
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 15
+ * iterator.hasNext(); // return true
+ * iterator.next();    // return 20
+ * iterator.hasNext(); // return false
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * next() and hasNext() should run in average O(1) time and uses O(h) memory,
+ * where h is the height of the tree.
+ * You may assume that next() call will always be valid, that is, there will be
+ * at least a next smallest number in the BST when next() is called.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
