65899db6d79bc2924724489782e67e9bbf814868
zhouchao3 <zhouchao3@xiaomi.com>
2019-04-01 15:08:07 +0800

    add 210 dfs

---
+++ 210.course-schedule-ii.0.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=210 lang=java
+ *
+ * [210] Course Schedule II
+ *
+ * https://leetcode.com/problems/course-schedule-ii/description/
+ *
+ * algorithms
+ * Medium (34.09%)
+ * Total Accepted:    136.8K
+ * Total Submissions: 401.4K
+ * Testcase Example:  '2\n[[1,0]]'
+ *
+ * There are a total of n courses you have to take, labeled from 0 to n-1.
+ * 
+ * Some courses may have prerequisites, for example to take course 0 you have
+ * to first take course 1, which is expressed as a pair: [0,1]
+ * 
+ * Given the total number of courses and a list of prerequisite pairs, return
+ * the ordering of courses you should take to finish all courses.
+ * 
+ * There may be multiple correct orders, you just need to return one of them.
+ * If it is impossible to finish all courses, return an empty array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2, [[1,0]] 
+ * Output: [0,1]
+ * Explanation: There are a total of 2 courses to take. To take course 1 you
+ * should have finished   
+ * course 0. So the correct course order is [0,1] .
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+ * Output: [0,1,2,3] or [0,2,1,3]
+ * Explanation: There are a total of 4 courses to take. To take course 3 you
+ * should have finished both     
+ * ⁠            courses 1 and 2. Both courses 1 and 2 should be taken after you
+ * finished course 0. 
+ * So one correct course order is [0,1,2,3]. Another correct ordering is
+ * [0,2,1,3] .
+ * 
+ * Note:
+ * 
+ * 
+ * The input prerequisites is a graph represented by a list of edges, not
+ * adjacency matrices. Read more about how a graph is represented.
+ * You may assume that there are no duplicate edges in the input
+ * prerequisites.
+ * 
+ * 
+ */
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) adj.add(i, new ArrayList<>());
+        for (int i = 0; i < prerequisites.length; i++) adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        boolean[] visited = new boolean[numCourses];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!topologicalSort(adj, i, stack, visited, new boolean[numCourses])) return new int[0];
+        }
+        int i = 0;
+        int[] result = new int[numCourses];
+        while (!stack.isEmpty()) {
+            result[i++] = stack.pop();
+        }
+        return result;
+    }
+
+    private boolean topologicalSort(List<List<Integer>> adj, int v, Stack<Integer> stack, boolean[] visited, boolean[] isLoop) {
+        if (visited[v]) return true;
+        if (isLoop[v]) return false;
+        isLoop[v] = true;
+        for (Integer u : adj.get(v)) {
+            if (!topologicalSort(adj, u, stack, visited, isLoop)) return false;
+        }
+        visited[v] = true;
+        stack.push(v);
+        return true;
+    }
+}
