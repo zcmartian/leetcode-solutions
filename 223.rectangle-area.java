93356aba9f596d9701a52aac3551109769561389
zhouchao3 <zhouchao3@xiaomi.com>
2019-04-03 14:32:21 +0800

    add 223

---
+++ 223.rectangle-area.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=223 lang=java
+ *
+ * [223] Rectangle Area
+ *
+ * https://leetcode.com/problems/rectangle-area/description/
+ *
+ * algorithms
+ * Medium (35.62%)
+ * Total Accepted:    85K
+ * Total Submissions: 238.5K
+ * Testcase Example:  '-3\n0\n3\n4\n0\n-1\n9\n2'
+ *
+ * Find the total area covered by two rectilinear rectangles in a 2D plane.
+ * 
+ * Each rectangle is defined by its bottom left corner and top right corner as
+ * shown in the figure.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+ * Output: 45
+ * 
+ * Note:
+ * 
+ * Assume that the total area is never beyond the maximum possible value of
+ * int.
+ * 
+ */
+class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int areaOfSqrA = (C-A) * (D-B);
+        int areaOfSqrB = (G-E) * (H-F);
+
+        int left = Math.max(A, E);
+        int right = Math.min(G, C);
+        int bottom = Math.max(F, B);
+        int top = Math.min(D, H);
+
+        //If overlap
+        int overlap = 0;
+        if(right > left && top > bottom)
+            overlap = (right - left) * (top - bottom);
+
+        return areaOfSqrA + areaOfSqrB - overlap;
+    }
+}
