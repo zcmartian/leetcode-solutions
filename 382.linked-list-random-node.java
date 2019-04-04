8315642cf95ba524af729df81640ae61a0c9a761
zhouchao3 <zhouchao3@xiaomi.com>
2019-04-04 17:55:04 +0800

    add 382

---
+++ 382.linked-list-random-node.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=382 lang=java
+ *
+ * [382] Linked List Random Node
+ *
+ * https://leetcode.com/problems/linked-list-random-node/description/
+ *
+ * algorithms
+ * Medium (48.89%)
+ * Total Accepted:    50.7K
+ * Total Submissions: 103.6K
+ * Testcase Example:  '["Solution","getRandom"]\n[[[1,2,3]],[]]'
+ *
+ * Given a singly linked list, return a random node's value from the linked
+ * list. Each node must have the same probability of being chosen.
+ * 
+ * Follow up:
+ * What if the linked list is extremely large and its length is unknown to you?
+ * Could you solve this efficiently without using extra space?
+ * 
+ * 
+ * Example:
+ * 
+ * // Init a singly linked list [1,2,3].
+ * ListNode head = new ListNode(1);
+ * head.next = new ListNode(2);
+ * head.next.next = new ListNode(3);
+ * Solution solution = new Solution(head);
+ * 
+ * // getRandom() should return either 1, 2, or 3 randomly. Each element should
+ * have equal probability of returning.
+ * solution.getRandom();
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    ListNode head;
+    Random random;
+
+    public Solution(ListNode h) {
+        head = h;
+        random = new Random();
+    }
+
+    public int getRandom() {
+
+        ListNode c = head;
+        int r = c.val;
+        for(int i=1;c.next != null;i++){
+
+            c = c.next;
+            if(random.nextInt(i + 1) == i) r = c.val;
+        }
+
+        return r;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
