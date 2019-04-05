aaeaf23ed0b610244aaa4c1d92c8886e6476ae99
zcmartian <zcmit@126.com>
2019-04-05 09:53:51 +0800

    add 260

---
+++ 260.single-number-iii.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=260 lang=java
+ *
+ * [260] Single Number III
+ *
+ * https://leetcode.com/problems/single-number-iii/description/
+ *
+ * algorithms
+ * Medium (56.49%)
+ * Total Accepted:    103.1K
+ * Total Submissions: 182.5K
+ * Testcase Example:  '[1,2,1,3,2,5]'
+ *
+ * Given an array of numbers nums, in which exactly two elements appear only
+ * once and all the other elements appear exactly twice. Find the two elements
+ * that appear only once.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:  [1,2,1,3,2,5]
+ * Output: [3,5]
+ * 
+ * Note:
+ * 
+ * 
+ * The order of the result is not important. So in the above example, [5, 3] is
+ * also correct.
+ * Your algorithm should run in linear runtime complexity. Could you implement
+ * it using only constant space complexity?
+ * 
+ */
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        // Pass 1 :
+        // Get the XOR of the two numbers we need to find
+        int diff = 0;
+        for (int num : nums) {
+            diff ^= num;
+        }
+        // Get its last set bit
+        diff &= -diff;
+
+        // Pass 2 :
+        int[] rets = {0, 0}; // this array stores the two numbers we will return
+        for (int num : nums)
+        {
+            if ((num & diff) == 0) // the bit is not set
+            {
+                rets[0] ^= num;
+            }
+            else // the bit is set
+            {
+                rets[1] ^= num;
+            }
+        }
+        return rets;
+    }
+}
