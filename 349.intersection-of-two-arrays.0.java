2b10be1f111df538965c938110837dd2955b91a6
zhouchao3 <zhouchao3@xiaomi.com>
2019-04-18 14:47:42 +0800

    add 349 binary

---
+++ 349.intersection-of-two-arrays.0.java
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=349 lang=java
+ *
+ * [349] Intersection of Two Arrays
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (51.96%)
+ * Total Accepted:    184.5K
+ * Total Submissions: 355.1K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two arrays, write a function to compute their intersection.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ * 
+ * 
+ * 
+ * 
+ */
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
