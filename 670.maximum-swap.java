eace5270744572662cfd1c0fc5b3e889cc4f6e4e
zcmartian <zcmit@126.com>
2019-05-16 00:42:14 +0800

    add 670

---
+++ 670.maximum-swap.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=670 lang=java
+ *
+ * [670] Maximum Swap
+ *
+ * https://leetcode.com/problems/maximum-swap/description/
+ *
+ * algorithms
+ * Medium (39.53%)
+ * Total Accepted:    35.4K
+ * Total Submissions: 89.6K
+ * Testcase Example:  '2736'
+ *
+ * 
+ * Given a non-negative integer, you could swap two digits at most once to get
+ * the maximum valued number. Return the maximum valued number you could get.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: 2736
+ * Output: 7236
+ * Explanation: Swap the number 2 and the number 7.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: 9973
+ * Output: 9973
+ * Explanation: No swap.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The given number is in the range [0, 10^8]
+ * 
+ * 
+ */
+class Solution {
+    public int maximumSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+        }
+
+        return num;
+    }
+}
