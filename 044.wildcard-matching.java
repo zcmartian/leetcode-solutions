08bfdf01ca8241f19a22cb2ad9c7413b2c123f92
zcmartian <zcmit@126.com>
2019-05-26 21:37:26 +0800

    modify 44

--- 044.wildcard-matching.java
+++ 044.wildcard-matching.java
@@ -13,73 +13,73 @@
  *
  * Given an input string (s) and a pattern (p), implement wildcard pattern
  * matching with support for '?' and '*'.
- * 
- * 
+ *
+ *
  * '?' Matches any single character.
  * '*' Matches any sequence of characters (including the empty sequence).
- * 
- * 
+ *
+ *
  * The matching should cover the entire input string (not partial).
- * 
+ *
  * Note:
- * 
- * 
+ *
+ *
  * s could be empty and contains only lowercase letters a-z.
  * p could be empty and contains only lowercase letters a-z, and characters
  * like ? or *.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input:
  * s = "aa"
  * p = "a"
  * Output: false
  * Explanation: "a" does not match the entire string "aa".
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input:
  * s = "aa"
  * p = "*"
  * Output: true
  * Explanation: '*' matches any sequence.
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input:
  * s = "cb"
  * p = "?a"
  * Output: false
  * Explanation: '?' matches 'c', but the second letter is 'a', which does not
  * match 'b'.
- * 
- * 
+ *
+ *
  * Example 4:
- * 
- * 
+ *
+ *
  * Input:
  * s = "adceb"
  * p = "*a*b"
  * Output: true
  * Explanation: The first '*' matches the empty sequence, while the second '*'
  * matches the substring "dce".
- * 
- * 
+ *
+ *
  * Example 5:
- * 
- * 
+ *
+ *
  * Input:
  * s = "acdcb"
  * p = "a*c?b"
  * Output: false
- * 
- * 
+ *
+ *
  */
 class Solution {
     public boolean isMatch(String s, String p) {
@@ -94,9 +94,9 @@ class Solution {
         for(int i=s.length()-1;i>=0;i--){
             for(int j=p.length()-1;j>=0;j--){
                 if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
-                        match[i][j]=match[i+1][j+1];
+                    match[i][j]=match[i+1][j+1];
                 else if(p.charAt(j)=='*')
-                        match[i][j]=match[i+1][j]||match[i][j+1];
+                    match[i][j]=match[i+1][j]||match[i][j+1];
                 else
                     match[i][j]=false;
             }
