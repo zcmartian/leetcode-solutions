f54b5942a802f7ceac09f35d3ac391c39c2d8238
zcmartian <zcmit@126.com>
2020-02-04 15:04:41 +0800

    format

--- 0001.two-sum.java
+++ 0001.two-sum.java
@@ -29,7 +29,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++) {
+        for(int i=0; i<nums.length;i++) {
             map.put(nums[i], i);
         }
         for(int i=0;i<nums.length;i++) {
