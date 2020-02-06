313e786747c887415fad069083c5159f78577b17
zcmartian <zcmit@126.com>
2020-02-06 11:27:23 +0800

    update 4

--- 0004.median-of-two-sorted-arrays.0.java
+++ 0004.median-of-two-sorted-arrays.0.java
@@ -12,56 +12,56 @@
  * Testcase Example:  '[1,3]\n[2]'
  *
  * There are two sorted arrays nums1 and nums2 of size m and n respectively.
- * 
+ *
  * Find the median of the two sorted arrays. The overall run time complexity
  * should be O(log (m+n)).
- * 
+ *
  * You may assume nums1 and nums2 cannot be both empty.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * nums1 = [1, 3]
  * nums2 = [2]
- * 
+ *
  * The median is 2.0
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * nums1 = [1, 2]
  * nums2 = [3, 4]
- * 
+ *
  * The median is (2 + 3)/2 = 2.5
- * 
- * 
+ *
+ *
  */
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (len1 > len2) return findMedianSortedArrays(nums2, nums1);
-        int k = (len1 + len2 + 1) / 2;
-        int l = 0;
-        int r = len1;
-        //find the m1 so that nums1[m1] >= nums2[m2 - 1]
-        while(l < r){
-            int m1 = l + (r - l) / 2;
-            int m2 = k - m1;
-            if (nums1[m1] < nums2[m2 - 1]){
-                l = m1 + 1;
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
+        int halfLen = (m + n + 1) / 2;
+        int imin = 0;
+        int imax = m;
+
+        while(imin < imax){
+            int i = imin + (imax - imin) / 2;
+            int j = halfLen - i;
+            if (nums1[i] < nums2[j - 1]) {
+                imin = i + 1;
             } else {
-                r = m1;
+                imax = i;
             }
         }
-        int m1 = l;
-        int m2 = k - l;
-        int c1 = Math.max(m1 <= 0 ? Integer.MIN_VALUE : nums1[m1 - 1],
-                         m2 <= 0? Integer.MIN_VALUE: nums2[m2 - 1]);
-        if ((len1 + len2) % 2 ==1) return c1;
-        int c2 = Math.min(m1 >= len1 ? Integer.MAX_VALUE : nums1[m1],
-                         m2 >= len2 ? Integer.MAX_VALUE: nums2[m2]);
+        int i = imin;
+        int j = halfLen - imin;
+        int c1 = Math.max(i <= 0 ? Integer.MIN_VALUE : nums1[i - 1],
+                         j <= 0? Integer.MIN_VALUE: nums2[j - 1]);
+        if ((m + n) % 2 ==1) return c1;
+        int c2 = Math.min(i >= m ? Integer.MAX_VALUE : nums1[i],
+                         j >= n ? Integer.MAX_VALUE: nums2[j]);
         return (c1 + c2) / 2.0;
     }
 }
